14965384427d1af570ffc7ef00c8c6c92c8ad388
Timi16 <timilehinolowu46@gmail.com>
2025-05-27 03:30:49 -0700

    fixing layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Colors } from '../../constants/Colors';
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false, animation: 'fade' }}>
+    <Stack 
+      screenOptions={{
+        headerTitle: '',
+        headerStyle: { 
+          backgroundColor: Colors.light.background 
+        },
+        headerShadowVisible: false,
+        headerShown: false, 
+        animation: 'fade'
+      }} 
+    >
       <Stack.Screen name="login" />
       <Stack.Screen name="signup" />
       <Stack.Screen name="forgot-password" />
