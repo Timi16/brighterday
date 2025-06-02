91dee7483a1a245f4400eb2ecdc25e029eaec5ab
Timi16 <timilehinolowu46@gmail.com>
2025-06-02 09:17:33 -0700

    Adding router fix in expo

--- context/SupabaseAuthContext.js
+++ context/SupabaseAuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 import { 
   supabase,
   signUpUser, 
